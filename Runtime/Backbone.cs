bc10488da69ba06b422466ae84396ace9bea53e8
Pedro Dominicé <pedro.dominice@outlook.com>
2022-04-06 20:33:14 +0200

    Add to Backbone interfaces IHoldSL and IHoldTM

--- Runtime/Backbone.cs
+++ Runtime/Backbone.cs
@@ -3,114 +3,110 @@ using HelvestSL;
 using StypeMachine;
 using UnityEngine;
 
-namespace BackboneFramework
+[RequireComponent(typeof(MonoServiceLocator))]
+[RequireComponent(typeof(MonoTypeMachine))]
+public abstract class Backbone : MonoBehaviour, IHoldSL, IHoldTM
 {
-	[RequireComponent(typeof(MonoServiceLocator))]
-	[RequireComponent(typeof(MonoTypeMachine))]
-	public abstract class Backbone : MonoBehaviour
+
+	#region Variables
+
+	public ServiceLocator SL => ServiceLocator.SL;
+
+	[field: SerializeField, Header("Backbone")]
+	public MonoServiceLocator ServiceLocator { get; private set; } = default;
+
+	public TypeMachine TM => StateMachine.TM;
+
+	[field: SerializeField]
+	public MonoTypeMachine StateMachine { get; private set; } = default;
+
+	[Header("Configs")]
+	[SerializeField]
+	private bool _dontDestroyOnLoad = true;
+
+	#endregion
+
+	#region Init
+
+	protected virtual void Awake()
 	{
-
-		#region Variables
-
-		public MonoServiceLocator SL => ServiceLocator;
-
-		[field: SerializeField, Header("Backbone")]
-		public MonoServiceLocator ServiceLocator { get; private set; } = default;
-
-		public MonoTypeMachine SM => StateMachine;
-
-		[field: SerializeField]
-		public MonoTypeMachine StateMachine { get; private set; } = default;
-
-		[Header("Configs")]
-		[SerializeField]
-		private bool _dontDestroyOnLoad = true;
-
-		#endregion
-
-		#region Init
-
-		protected virtual void Awake()
+		if (_dontDestroyOnLoad)
 		{
-			if (_dontDestroyOnLoad)
-			{
-				DontDestroyOnLoad(gameObject);
-			}
+			DontDestroyOnLoad(gameObject);
 		}
-
-		protected virtual void OnEnable()
-		{
-			SL.Add(this);
-		}
-
-		protected virtual void OnDisable()
-		{
-			SL.Remove(this);
-		}
-
-		#endregion
-
-		#region Service Locator
-
-		public bool Add<T>(T instance) where T : MonoBehaviour
-		{
-			return SL.Add(instance);
-		}
-
-		public bool Remove<T>() where T : class
-		{
-			return SL.Remove<T>();
-		}
-
-		public bool TryGetIfNull<T>(ref T instance, Action<T> callback = null) where T : class
-		{
-			return SL.TryGetIfNull(ref instance, callback);
-		}
-
-		#endregion
-
-		#region State Machine
-
-		public void SetState<t>() where t : MonoBehaviour
-		{
-			SM.SetState<t>();
-		}
-
-		public void SetState<t>(t instance) where t : MonoBehaviour
-		{
-			SM.SetState(instance);
-		}
-
-		public void SetState(Type type)
-		{
-			SM.SetState(type);
-		}
-
-		public Type State
-		{
-			get => SM.State;
-			set => SM.State = value;
-		}
-
-		#endregion
-
-		#region OnValidate
-
-		protected virtual void OnValidate()
-		{
-			if (ServiceLocator == null)
-			{
-				ServiceLocator = GetComponent<MonoServiceLocator>();
-			}
-
-			if (StateMachine == null)
-			{
-				StateMachine = GetComponent<MonoTypeMachine>();
-			}
-		}
-
-		#endregion
-
 	}
+
+	protected virtual void OnEnable()
+	{
+		SL.Add(this);
+	}
+
+	protected virtual void OnDisable()
+	{
+		SL.Remove(this);
+	}
+
+	#endregion
+
+	#region Service Locator
+
+	public bool Add<T>(T instance) where T : MonoBehaviour
+	{
+		return SL.Add(instance);
+	}
+
+	public bool Remove<T>() where T : class
+	{
+		return SL.Remove<T>();
+	}
+
+	public bool TryGetIfNull<T>(ref T instance, Action<T> callback = null) where T : class
+	{
+		return SL.TryGetIfNull(ref instance, callback);
+	}
+
+	#endregion
+
+	#region State Machine
+
+	public void SetState<t>() where t : MonoBehaviour
+	{
+		TM.SetState<t>();
+	}
+
+	public void SetState<t>(t instance) where t : MonoBehaviour
+	{
+		TM.SetState(instance);
+	}
+
+	public void SetState(Type type)
+	{
+		TM.SetState(type);
+	}
+
+	public Type State
+	{
+		get => TM.State;
+		set => TM.State = value;
+	}
+
+	#endregion
+
+	#region OnValidate
+
+	protected virtual void OnValidate()
+	{
+		if (ServiceLocator == null)
+		{
+			ServiceLocator = GetComponent<MonoServiceLocator>();
+		}
+
+		if (StateMachine == null)
+		{
+			StateMachine = GetComponent<MonoTypeMachine>();
+		}
+	}
+
+	#endregion
 
 }
